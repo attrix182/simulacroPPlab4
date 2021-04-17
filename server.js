4bc927089b1ea3a3bb3028a027a2c94918eeff04
attrix <lucho.bernal@hotmail.com>
2021-04-17 19:48:54 -0300

    fixPackage

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.listen(process.env.PORT || 8080);
 app.get('/*', function (req, res) {
 
     res.sendFile(path.join(__dirname + '/dist/tpPeliculas/index.html'));
+
 });
 
 console.log("Build");
